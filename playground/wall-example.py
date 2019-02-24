35c0af9e11a6a6dd36923d3db428e48dbf8d9046
Yongsub Chung <litdream@gmail.com>
2019-02-24 16:34:55 -0500

    beginning of Maze-wall example.

--- playground/wall-example.py
+++ playground/wall-example.py
@@ -1,15 +1,25 @@
+"""
+This is example similar to move_with_walls_example.py
+
+"""
+
 import pygame
 import pycolor
+
+class Player(pygame.sprite.Sprite):
+    pass
+
+class Wall(pygame.sprite.Sprite):
+    pass
+
 
 def main():
     pygame.init()
     size = [700, 500]
     screen = pygame.display.set_mode(size)
-    pygame.display.set_caption("Bouncing Rectangle")
+    pygame.display.set_caption("Maze-like Wall example")
     clock = pygame.time.Clock()
     done = False
-    rectX, rectY = 50,50
-    rect_Xdelta, rect_Ydelta = 2,2
 
     while not done:
         for ev in pygame.event.get():
@@ -20,18 +30,8 @@ def main():
                     print("pressed: CTRL + q")
                     done = True
 
-        rectX += rect_Xdelta
-        rectY += rect_Ydelta
-
-        if rectY > 450 or rectY < 0:
-            rect_Ydelta = -rect_Ydelta
-        if rectX > 650 or rectX < 0:
-            rect_Xdelta = -rect_Xdelta
-
-        screen.fill(pycolor.black)
-        pygame.draw.rect(screen, pycolor.white, [rectX, rectY, 50,50])
-        pygame.draw.rect(screen, pycolor.red, [rectX+10, rectY+10, 30,30])
-
+        # TODO: update logic
+                    
         clock.tick(60)
         pygame.display.flip()
     pygame.quit()
