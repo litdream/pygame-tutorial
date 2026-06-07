438741a067c9a71fb7b32c3178ccdc4ef9118003
Ray Chung <rchung725@gmail.com>
2026-06-07 06:41:17 -0400

    messed the first try

--- multilang/tetris/my-study/main.py
+++ multilang/tetris/my-study/main.py
@@ -24,8 +24,8 @@ COLORS = {
 CELL_SIZE = 40
 STAGE_WIDTH = 10
 STAGE_HEIGHT = 20
-SCREEN_WIDTH = CELL_SIZE * STAGE_WIDTH
-SCREEN_HEIGHT = CELL_SIZE * STAGE_HEIGHT
+SCREEN_WIDTH = CELL_SIZE * ( STAGE_WIDTH + 2 )
+SCREEN_HEIGHT = CELL_SIZE * ( STAGE_HEIGHT + 1 )
 
 
 
@@ -163,16 +163,21 @@ def main():
     block_types = list("IOTJLSZ")
 
     # initial
+    fall_speed = 500
     curblk_type = random.choice(block_types)
     curblk_lst = SHAPES[curblk_type]
     curblk_idx = 0
+    (blk_x, blk_y) = ( 5 - len(SHAPES[curblk_type][0])//2 ,0)
     stage = get_stage()
 
+    last_update_time = pygame.time.get_ticks()  # Get time in millis.
+    
     while running:
         screen.fill(BLACK)
 
         delta_time = clock.tick(60)
         current_time = pygame.time.get_ticks()  # Get time in millis.
+        #print(last_update_time, current_time)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -182,11 +187,51 @@ def main():
                 if event.key == pygame.K_x:     # CTRL-X:  quit game.
                     if event.mod & pygame.KMOD_CTRL:
                         running = False
+                if event.key == pygame.K_SPACE:
+                    curblk_idx += 1
+                    curblk_idx %= len(curblk_lst)
 
-        # DRAW BLOCKS
+
+        # DRAW block
+        shape = curblk_lst[curblk_idx]
+        #print(shape)
+        #print(blk_x, blk_y)
+        
+        for dy,blk_row in enumerate(shape):
+            for dx,val in enumerate(blk_row):
+                if val == 1:
+                    rect = pygame.Rect( (blk_x + dx)*CELL_SIZE, blk_y + dy*CELL_SIZE, CELL_SIZE, CELL_SIZE )
+                    pygame.draw.rect(screen, GRAY, rect)
+                
+                
+        # DRAW Stage
+        y = 0
+        for stage_row in stage:
+            for dx,val in enumerate(stage_row):
+                if val == 1:
+                    rect = pygame.Rect( dx*CELL_SIZE, y, CELL_SIZE, CELL_SIZE )
+                    pygame.draw.rect(screen, GRAY, rect)
+            y += CELL_SIZE
+            
         pygame.display.flip()
-    pygame.display.flip()
 
+        if current_time - last_update_time > fall_speed:
+            blk_y += CELL_SIZE
+            last_update_time = current_time
+
+        # Collision:  new init.
+        if not can_block_apply(stage, curblk_lst, curblk_idx, blk_x, blk_y + CELL_SIZE):
+            for dy,blk_row in enumerate(shape):
+                for dx,val in enumerate(blk_row):
+                    if val == 1:
+                        print(blk_x, blk_y, dx, dy)
+                        stage[blk_y+dy][blk_x+dx] = 1
+                                    
+            curblk_type = random.choice(block_types)
+            curblk_lst = SHAPES[curblk_type]
+            curblk_idx = 0
+            (blk_x, blk_y) = ( 5 - len(SHAPES[curblk_type][0])//2 ,0)
+        
 
 if __name__ == '__main__':
     main()
